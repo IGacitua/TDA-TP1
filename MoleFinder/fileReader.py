32515ee23d5eec74e2ca32cc095f56379c6bbf86
IGacitua <ezmagacitua@gmail.com>
2025-03-25 19:19:45 -0300

    Creada función de utilidad fileReader()

---
+++ MoleFinder/fileReader.py
@@ -0,0 +1,28 @@
+import sys
+
+# Lee el archivo dado por argumento
+# Formato como ../"Archivos Test"
+# Devuelve una dupla.
+# Primer segmento es un array de duplas (timestamp, errorMargin)
+# Segundo segmento es un array de los tiempos de operaciones
+def fileReader():
+    # Get file from argument
+    filePath = sys.argv[1]
+    file = open(filePath, 'r')
+    firstLine = file.readline()
+
+    elementCount = 0
+    # Ignore comment if exists
+    if (firstLine[0] == '#'):
+        elementCount = int(file.readline())
+    else:
+        elementCount = int(firstLine)
+    
+    timeStamps = []
+    operations = []
+    for i in range(elementCount):
+        line = file.readline().split(",")
+        timeStamps.append((line[0], line[1].strip('\n')))
+    for i in range(elementCount):
+        operations.append(int(file.readline()))
+    return timeStamps, operations
