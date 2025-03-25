e24f82846938280aa472aef2a9b48236186ceebc
IGacitua <ezmagacitua@gmail.com>
2025-03-25 20:35:35 -0300

    Bugfix menor. El programa da falso negativo, no encuentro el error. Probar con "100-es.txt"

--- MoleFinder/moleFinder.py
+++ MoleFinder/moleFinder.py
@@ -7,26 +7,26 @@ def moleFinder():
     for i in range(len(operations)):
         operationFound = checkContains(operations[i], timestamps, i)
         if (operationFound == -1):
+            print(f"Found false at {i} iteration.")
             return False
         else:
             timestamps[i] = None
     return True
 
 def checkContains(operation, timestamps, start):
-    contentArray = []
+    lowestFound = -1
     for i in range(start, len(timestamps)):
         if (timestamps[i] is None):
             continue
         rangeLower = timestamps[i][0] - timestamps[i][1]
         rangeUpper = timestamps[i][0] + timestamps[i][1]
-        if (operation > rangeLower) and (operation < rangeUpper):
-            contentArray.append(i)
-    marginSort(contentArray)
-    if (len(contentArray) == 0):
-        return -1
-    else:
-        return contentArray[0]
-    
+        if (operation >= rangeLower) and (operation <= rangeUpper):
+            if (lowestFound == -1):
+                lowestFound = i
+            else:
+                if (timestamps[lowestFound][1] > timestamps[i][1]):
+                    lowestFound = i
+    return lowestFound
 
 if __name__ == "__main__":
     print(moleFinder())
