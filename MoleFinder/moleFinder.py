1369913809bb770840c2a5a31d483bc8b3dcb4e9
IGacitua <ezmagacitua@gmail.com>
2025-03-26 21:57:52 -0300

    First iteration of new algorithm. Has false positives.

--- MoleFinder/moleFinder.py
+++ MoleFinder/moleFinder.py
@@ -3,40 +3,36 @@ from sorters import *
 
 def moleFinder():
     timestamps, operations = fileReader() # Crea ambas listas
-    timestampSort(timestamps) # Ordena timestamps por el margen izquierdo, como habiamos hablado. POSIBLE ZONA DE ERROR
-    for i in range(len(operations)):
-        operationFound = checkContains(operations[i], timestamps, i)
-        if (operationFound == -1):
-            # No encontró nada.
-            print(f"Found false at {i} iteration.")
-            return False # No es la rata
-        else:
-            timestamps[i] = None # Elimina de las posibilidades a esa timestamp
+    appareances = createAppareanceList(timestamps, operations)
+    lengthSort(appareances)
+    foundTimestamps = []
+    for i in range(len(appareances)):
+        closest = findClosest(operations[i], appareances[i], foundTimestamps)
+        if (closest == -1):
+            return False
     return True
 
-def checkContains(operation, timestamps, start):
-    lowestFound = -1 # -1 -> nada
-    for i in range(start, len(timestamps)):
-        if (timestamps[i] is None):
-            # Saltear si ya fue nulificada
-            continue
-        rangeLower = timestamps[i][0] - timestamps[i][1] # T - E
-        rangeUpper = timestamps[i][0] + timestamps[i][1] # T + E
-        if (operation >= rangeLower) and (operation <= rangeUpper):
-            # Está dentro del rango. Inclusivo.
-            if (lowestFound == -1):
-                # No se encontró nada otro, sirve.
-                lowestFound = i
-            else:
-                if (timestamps[lowestFound][1] > timestamps[i][1]):
-                    # Busca cual es el que tiene menor margen de error
-                    # Debería ser el mas cercano a la operación
-                    # Puede ser malo?
-                    lowestFound = i
-                elif (timestamps[lowestFound][1] == timestamps[i][1]):
-                    # Posible zona de error. No parece pero hay que agregar un criterio mas
-                    print("Oh?")
-    return lowestFound
+    
+def createAppareanceList(timestamps, operations):
+    appareances = [[]] * len(operations) 
+    for o in range(len(appareances)):
+        operation = operations[o]
+        for t in range(len(appareances)):
+            lowerBound = timestamps[t][0] - timestamps[t][1]
+            upperBound = timestamps[t][0] + timestamps[t][1]
+            if (operation > lowerBound) and (operation < upperBound):
+                appareances[o].append(timestamps[t])
+    return appareances
+
+def findClosest(operationTime, timestamps, foundTimestamps):
+    distanceSort(timestamps, operationTime)
+    for i in range(len(timestamps)):
+        if i not in foundTimestamps:
+            foundTimestamps.append(i)
+            return i
+    return -1
+
+
 
 if __name__ == "__main__":
     print(moleFinder())
--- MoleFinder/sorters.py
+++ MoleFinder/sorters.py
@@ -1,30 +1,72 @@
+import math
 # Temp bubble sort. Reemplazar por quicksort
 
-
-# Ordena el array en función de borde izquierdo
-def timestampSort(timestamps):
-    for i in range(len(timestamps)):
+# Ordena la lista en función de borde izquierdo
+# DEPRECATED
+def timestampSort(list):
+    for i in range(len(list)):
         swapped = False
-        for j in range(0, len(timestamps) - i - 1):
-            differenceOne = timestamps[j][0] - timestamps[j][1]
-            differenceTwo = timestamps[j+1][0] - timestamps[j+1][1]
+        for j in range(0, len(list) - i - 1):
+            differenceOne = list[j][0] - list[j][1]
+            differenceTwo = list[j+1][0] - list[j+1][1]
             if (differenceOne > differenceTwo):
-                aux = timestamps[j]
-                timestamps[j] = timestamps[j+1]
-                timestamps[j+1] = aux
+                aux = list[j]
+                list[j] = list[j+1]
+                list[j+1] = aux
                 swapped = True
         if swapped is False:
             break
 
-# Ordena el array en función del margen de error
-def marginSort(timestamps):
-    for i in range(len(timestamps)):
+# Ordena la lista en función del margen de error
+# DEPRECATED
+def marginSort(list):
+    for i in range(len(list)):
         swapped = False
-        for j in range(0, len(timestamps) - i - 1):
-            if (timestamps[j][1] > timestamps[j+1][1]):
-                aux = timestamps[j]
-                timestamps[j] = timestamps[j+1]
-                timestamps[j+1] = aux
+        for j in range(0, len(list) - i - 1):
+            if (list[j][1] > list[j+1][1]):
+                aux = list[j]
+                list[j] = list[j+1]
+                list[j+1] = aux
+                swapped = True
+        if swapped is False:
+            break
+
+# Ordena la lista en función del centro del intervalo
+# DEPRECATED
+def centerSort(list):
+    for i in range(len(list)):
+        swapped = False
+        for j in range(0, len(list) - i - 1):
+            if (list[j][0] > list[j+1][0]):
+                aux = list[j]
+                list[j] = list[j+1]
+                list[j+1] = aux
+                swapped = True
+        if swapped is False:
+            break
+
+#Ordena la lista en función de la distancia entre el centro del intervalo y la operación
+def distanceSort(list, operationTime):
+    for i in range(len(list)):
+        swapped = False
+        for j in range(0, len(list) - i - 1):
+            if (abs(operationTime - list[j][0]) > abs(operationTime - list[j+1][0])):
+                aux = list[j]
+                list[j] = list[j+1]
+                list[j+1] = aux
+                swapped = True
+        if swapped is False:
+            break
+
+# Ordena una lista de listas en base a la longitud de la lista
+def lengthSort(list):
+    for i in range(len(list)):
+        swapped = False
+        for j in range(0, len(list) - i - 1):
+            if (len(list[j]) > len(list[j+1])):
+                aux = list[j]
+                list[j] = list[j+1]
+                list[j+1] = aux
                 swapped = True
         if swapped is False:
             break
