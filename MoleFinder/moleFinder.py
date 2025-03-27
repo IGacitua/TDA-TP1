1dcfd8766cff23f806c334da81922e68ccd06bc2
IGacitua <ezmagacitua@gmail.com>
2025-03-26 22:25:20 -0300

    Debug prints. Replace the debug global variable to disable.

--- MoleFinder/moleFinder.py
+++ MoleFinder/moleFinder.py
@@ -1,16 +1,22 @@
 from fileReader import *
 from sorters import *
+import time # For testing
+
+debug = True
+startTime = time.time()
 
 def moleFinder():
-    debug = True
+
     timestamps, operations = fileReader() # Crea ambas listas
     appareances = createAppareanceList(timestamps, operations)
+    if (debug is True):
+        print("Appareances done.")
     lengthSort(appareances)
     foundTimestamps = []
     i = 0
     for i in range(len(appareances)):
         if (debug is True):
-            print(f"Iteration {i}")
+            print(f"Iteration {i}, {(startTime - time.time()):.2f}s since start.")
         closest = findClosest(operations[i], appareances[i], foundTimestamps)
         if (closest == -1):
             return False
@@ -24,6 +30,8 @@ def moleFinder():
 def createAppareanceList(timestamps, operations):
     appareances = [[]] * len(operations) 
     for o in range(len(appareances)):
+        if (debug is True):
+            print(f"Operation {o}, {(startTime - time.time()):.2f}s since start.")
         operation = operations[o]
         for t in range(len(appareances)):
             lowerBound = timestamps[t][0] - timestamps[t][1]
@@ -33,9 +41,11 @@ def createAppareanceList(timestamps, operations):
     return appareances
 
 def findClosest(operationTime, timestamps, foundTimestamps):
-    #distanceSort(timestamps, operationTime)
-    marginSort(timestamps)
+    distanceSort(timestamps, operationTime)
+    #marginSort(timestamps)
     for i in range(len(timestamps)):
+        if (debug is True):
+            print(f"Timestamp {i}: {timestamps[i]}")
         if timestamps[i] not in foundTimestamps:
             foundTimestamps.append(timestamps[i])
             return i
@@ -45,3 +55,4 @@ def findClosest(operationTime, timestamps, foundTimestamps):
 
 if __name__ == "__main__":
     print(moleFinder())
+    print(f"{(startTime - time.time()):.2f}s since start.")
--- MoleFinder/sorters.py
+++ MoleFinder/sorters.py
@@ -1,6 +1,8 @@
 import math
 # Temp bubble sort. Reemplazar por quicksort
 
+debug = True
+
 # Ordena la lista en función de borde izquierdo
 # DEPRECATED
 def timestampSort(list):
@@ -48,6 +50,8 @@ def centerSort(list):
 #Ordena la lista en función de la distancia entre el centro del intervalo y la operación
 def distanceSort(list, operationTime):
     for i in range(len(list)):
+        if (debug is True):
+            print(f"Sort Nº{i}")
         swapped = False
         for j in range(0, len(list) - i - 1):
             if (abs(operationTime - list[j][0]) > abs(operationTime - list[j+1][0])):
