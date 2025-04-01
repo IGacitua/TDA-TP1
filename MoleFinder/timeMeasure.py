1d9ca70415258ed94748e9f404fe4248db398dca
IGacitua <ezmagacitua@gmail.com>
2025-04-01 18:57:49 -0300

    Style changes. Still not sure why it only works on VSCode Debugger

--- MoleFinder/timeMeasure.py
+++ MoleFinder/timeMeasure.py
@@ -1,9 +1,11 @@
-from matplotlib import pyplot as plt
-from moleFinder import *
-from fileReader import *
-from intervalGenerator import *
-from scipy.optimize import curve_fit
 import time
+from moleFinder import moleFinder
+from fileReader import fileReader
+from intervalGenerator import fileCreator
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+
+debug = False
 
 # Ejecuta FUNCTION con PARAMS, devuelve el tiempo de ejecución
 def measureTime(function, *params):
@@ -18,31 +20,36 @@ def averageTime(function, precision, *params):
         sum += measureTime(function, *params)
     return sum / precision
 
-# Genera promedios para archivos con LENGTH intervalos
-def dataGenerator(length):
-    timestamps, operations = fileReader(fileCreator(True, length))
-    average = averageTime(moleFinder, 10, timestamps, operations, [])
-    print(f"Average for length {length}: {average}")
-    return average
-
 # Funcion que utiliza curve_fit para aproximar tiempos
 def f(x, c1, c2):
     return c1 * x + c2
 
-def plotTime(x_values):
+def plotTime(x_values, precision, generatorSteps):
     y_values = []
     for i in x_values:
-        y_values.append(dataGenerator(i))
+        timestamps, operations = fileReader(fileCreator(True, i))
+        y_values.append(averageTime(moleFinder, precision, timestamps, operations, []))
     c, pcov = curve_fit(f, x_values, y_values)
-    print(f"X: {x_values}")
-    print(f"Y: {y_values}")
-    print(f"C: {c}")
-    ax: plt.Axes
-    fig, ax = plt.subplots()
-    ax.plot(x_values, y_values, label = "Original")
-    ax.plot(x_values, [c[0] * n + c[1] for n in x_values], 'r--', label="Ajuste")
-    ax.legend()
+    if debug:
+        print(f"X: {x_values}")
+        print(f"Y: {y_values}")
+        print(f"C: {c}")
+
+    xInterval = generatorSteps * (len(x_values) // 10) # Interval at which the X axis has markers
+
+    plt.plot(x_values, y_values, label = "Original", c = "Black", linestyle="solid", marker=".", alpha = 0.5)
+    plt.plot(x_values, [c[0] * n + c[1] for n in x_values], label = "Adjusted", c = "Red", linestyle="dotted")
+    plt.xlabel("Interval count")
+    plt.ylabel("Execution Time (ms)")
+    plt.xticks(range(0, x_values[-1], xInterval)) 
+    plt.legend()
     plt.show()
 
-plotTime([10,20,30,40,50])
-pause = input()
+
+if __name__ == '__main__':
+    steps = 10
+    interval = [i for i in range(0,1000, steps)]
+    precision = 25
+
+    plotTime(interval, precision, steps)
+    pause = input()
