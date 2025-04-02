c30d52283e0215ae048bc248526d218eaa572a98
IGacitua <ezmagacitua@gmail.com>
2025-04-02 19:15:51 -0300

    More documentation in intervalGenerator, and less imports

--- src/intervalGenerator.py
+++ src/intervalGenerator.py
@@ -1,42 +1,55 @@
-import sys
-import random
+import sys # Recieve command line parameters
+from random import randrange as rng # Generate the data
+from random import shuffle # Shuffle arrays before output to file
 
+# Creates intervals and operations
+# If isRat, matches each operation with an interval
 def intervalGenerator(size, isRat):
     intervals, operations = [], []
     for i in range(size):
+        # Generates time intervals (Timestamp, Error Margin)
         interval = {}
-        timeStamp   = random.randrange(2, 1000) # Timestamp > 1
-        errorMargin = random.randrange(1, timeStamp) # Timestamp > Error Margin > 0
-        interval['ts'] = timeStamp
-        interval['er'] = errorMargin
-        intervals.append(interval)
+        interval['ts'] = rng(2, 1000) # Timestamp > 1
+        interval['er'] = rng(1, interval['ts']) # Timestamp > Error Margin > 0
+        intervals.append(interval) # Adds created interval to list
     if isRat:
+        # Generates ONE operation within each interval
         for o in range(size):
             lowerBound = intervals[o]['ts'] - intervals[o]['er']
             upperBound = intervals[o]['ts'] + intervals[o]['er']
-            operations.append(random.randrange(lowerBound, upperBound))
+            operations.append(rng(lowerBound, upperBound))
     else:
+        # Generates operations randomly
         for o in range(size-1):
-            operations.append(random.randrange(1000))
+            operations.append(rng(1000))
     return intervals, operations
 
+# Creates a file with the contents from intervalGenerator()
+# outPath is optional
 def fileCreator(isRat, size, results, outPath = "src/default_output.txt"):
-    intervals, operations = intervalGenerator(size, isRat)
-    mainFile = open(outPath, 'w')
-    mainFile.write(f"# File generated automatically. Size {size}. Rat = {isRat}\n")
-    mainFile.write(f"{str(size)}\n")
+    intervals, operations = intervalGenerator(size, isRat) # Generates the data
+    mainFile = open(outPath, 'w') 
+    mainFile.write(f"# File generated automatically. Size {size}. Rat = {isRat}\n") # First line is a comment
+    mainFile.write(f"{str(size)}\n") # Second line is the interval/operation count
+
+    # Crea un archivo diciendo en cual intervalo va cada operación
     if (isRat and results):
         subFile = open(outPath + ' - RESULTS', 'w')
         for i in range(size):
             line = f"{operations[i]} --> {intervals[i]['ts']} +/- {intervals[i]['er']} \n"
             subFile.write(line)
-    random.shuffle(intervals)
-    random.shuffle(operations)
+
+    # Desordena ambas listas
+    shuffle(intervals)
+    shuffle(operations)
+
+    # Crea el resto archivo principal
     for i in range(size):
         mainFile.write(f"{intervals[i]['ts']},{intervals[i]['er']}\n")
     for i in range(size):
         mainFile.write(f"{operations[i]}\n")
-    return outPath
+    
+    return outPath # Devuelve donde es guarda el archivo final, para poder mandar directamente a otra función
 
 if __name__ == "__main__":
     # Input:
@@ -44,7 +57,9 @@ if __name__ == "__main__":
     # Int -> 0 si no rata, 1 si rata
     # Size -> Numero de intervalos
     # Path -> Nombre del archivo out
+    
     rat = True
     if (sys.argv[1] == 0):
         rat = False
+
     fileCreator(rat, int(sys.argv[2]), True, outPath = sys.argv[3])
