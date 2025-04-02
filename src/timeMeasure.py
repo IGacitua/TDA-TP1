6e63ae6b042fae15bd9ec085198b368e5f8fcdcc
IGacitua <ezmagacitua@gmail.com>
2025-04-02 18:18:32 -0300

    timeMeasure now recieves arguments from command line. Changed file naming to prevent two instances of the same program interfering with each other. Added new naming convention to .gitignore

--- src/timeMeasure.py
+++ src/timeMeasure.py
@@ -1,16 +1,15 @@
 # Python libraries
-from os import remove as deleteFile
-from sys import maxsize
-from random import randrange as rng
-from time import time as currentTime
-from math import log
+from os import remove as deleteFile # Delete excess files
+from sys import argv # Recieve command line parameters
+from time import time as currentTime # Measure time
+from datetime import datetime # To name files with current hour
 # External libraries
-from matplotlib import pyplot as plt
-from scipy.optimize import curve_fit
+from matplotlib import pyplot as plt # Graphics
+from scipy.optimize import curve_fit # Adjust data to function
 # Internal libraries
 from moleFinder import moleFinder as measurableFunction # AS para poder reutilizar el codigo
-from intervalGenerator import fileCreator
-from fileReader import fileReader
+from intervalGenerator import fileCreator # Generates disposable data
+from fileReader import fileReader # Reads the generated data
 
 debug = True
 
@@ -18,11 +17,11 @@ debug = True
 def digits(num):
     return len(str(num))
 
-# Ejecuta FUNCTION con PARAMS, devuelve el tiempo de ejecución
+# Ejecuta FUNCTION con PARAMS, devuelve el tiempo de ejecución en milisegundo
 def measureTime(function, *params):
     startTime = currentTime() # Current time
     function(*params) # Executes function
-    return currentTime() - startTime # Returns elapsed time
+    return (currentTime() - startTime) * 1000 # Returns elapsed time
 
 # Mide el tiempo de ejecución de FUNCTION con PARAMS, PRECISION veces y devuelve el promedio
 def averageTime(function, precision, *params):
@@ -39,18 +38,20 @@ def quadratic(x, c1, c2, c3):
 # Plotea la función original, y su curve_fit utilizando F
 # GENERATORSTEPS es cada cuanto varía cada valor de X (0,10,20,30,40,50) tiene STEPS de 10. Se utiliza para los ejes.
 def plotTime(x_values, precision, generatorSteps, f):
+    p_id = datetime.now().microsecond # ID so you can run the program multiple times in paralel
     y_values_original = [] # Variables dependientes de la función sin ajustar
 
     width = digits(x_values[-1]) # Usado exclusivamente para prints
-    file = '' # Creo la variable fuera del loop para solo tener que borrar una vez el archivo
     for i in range(len(x_values)):
-        file = fileCreator(True, x_values[i], False) # Al usar siempre el mismo archivo no necesito setear semilla
+        dt = f"{datetime.now().hour}.{datetime.now().minute}.{datetime.now().second}"
+        # Nombro el archivo para poder ejecutar dos instancias del programa sin problema
+        file = fileCreator(True, x_values[i], False, outPath=f"Automatic {p_id}-{dt}.txt") # Al usar siempre el mismo archivo no necesito setear semilla
         timestamps, operations = fileReader(file) # Obtiene los parametros utilizados por moleReader
         # NOTA: De reutilizar la función, cambiar linea de arriba
         y_values_original.append(averageTime(measurableFunction, precision, timestamps, operations, []))
         if debug:
-            print(f"{x_values[i]:>0{width}}: {y_values_original[-1]:.5f}s")
-    deleteFile(file) # Borra el archivo luego de uso. El loop sobreescribe asi que esto puede estar fuera
+            print(f"{x_values[i]:>0{width}}: {y_values_original[-1]:.2f}ms")
+        deleteFile(file) # Borra el archivo luego de uso.
 
     c, pcov = curve_fit(f, x_values, y_values_original) # Obtiene la variación de X/Y en base a F
     y_values_adjusted = [f(n,c[0],c[1], c[2]) for n in x_values] # Obtiene los valores ajustados en base a variación
@@ -66,17 +67,28 @@ def plotTime(x_values, precision, generatorSteps, f):
 
     # LABELS
     plt.xlabel("Interval count")
-    plt.ylabel("Execution Time (s)")
-    plt.title(f"Execution times for {len(x_values) - 1} intervals.", loc = 'left')
-    xInterval = generatorSteps * (len(x_values) // 10) # Intervalo en el cual el eje X tiene marcadores
-    plt.xticks(range(0, x_values[-1] + xInterval, xInterval)) # Aplico lo de la linea de arriba
+    plt.ylabel("Execution Time (ms)")
+    plt.title(f"Average times for {len(x_values) - 1} executions.", loc = 'left')
+    
+    # X/Y axis marks.
+    highestX = x_values[-1]
+    highestY = max(y_values_original[-1], y_values_adjusted[-1])
+    xTicks, yTicks = [],[]
+    for i in range(11):
+        xTicks.append(round((highestX / 10) * i, 2))
+        yTicks.append(round((highestY / 10) * i, 2))
+    plt.xticks(xTicks)
+    plt.yticks(yTicks)
+
+    plt.legend()
     plt.show() # Muestro el gráfico
 
 
 if __name__ == '__main__':
-    steps = 100
-    precision = 50
-    intervalCount = 10000
-    interval = [i for i in range(0,intervalCount + 1, steps)]
+    # ARGUMENT 1: Steps
+    # ARGUMENT 2: Precision
+    # ARGUMENT 3: Maximum Intervals
+    print(f"Running {int(argv[3]) // int(argv[1])} iterations with {argv[2]} precision.")
+    interval = [i for i in range(0, int(argv[3]) + 1, int(argv[1]))]
 
-    plotTime(interval, precision, steps, quadratic)
+    plotTime(interval, int(argv[2]), int(argv[1]), quadratic)
